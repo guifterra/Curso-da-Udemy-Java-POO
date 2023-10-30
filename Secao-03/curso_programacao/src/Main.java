877e4430eb73beecab7746cb12a7fb847e5816cb
guifterra <guilhermeterra391@gmail.com>
2023-10-29 22:02:21 -0200

    Curso de Java - Udeny

---
+++ Secao-03/curso_programacao/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ *  Nome: Guilherme Fabiano Terra
+ *  S3: 1° Programa - Imprimindo "Ola mundo!"
+ *  
+ */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.print("\n Ola mundo!");
+
+	}
+
+}
